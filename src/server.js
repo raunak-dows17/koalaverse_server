7d116b6ce91d84b3cba5631d48beaee32491d6f4
Raunak Pandey <praunak82@gmail.com>
2024-03-15 13:52:33 +0530

    cors policy solved

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 require("../config/db");
 const userRouter = require("./routes/userRoutes");
@@ -10,6 +11,7 @@ app.get("/", (req, res) => {
   res.send("Welcome to Taleverse's Serverse");
 });
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", userRouter);
 
